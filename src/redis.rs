770413113c2b7801730b21093b006a68ccffc252
Lee Smet <lee.smet@hotmail.com>
2022-06-14 18:14:53 +0200

    Implement redis cluster client
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- src/redis.rs
+++ src/redis.rs
@@ -1,10 +1,44 @@
-use redis_cluster_async::{redis::Cmd, Client};
-use std::net::SocketAddr;
+use futures_util::StreamExt;
+use redis_cluster_async::{
+    redis::{AsyncCommands, Cmd},
+    Client,
+};
+use serde::Deserialize;
+use trust_dns_server::client::rr::LowerName;
+
+use std::{fmt::Display, net::SocketAddr, str::FromStr};
 
 use crate::storage::Storage;
 
 pub struct RedisClusterClient {
     client: Client,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ConnectionInfo {
+    pub user: Option<String>,
+    pub password: Option<String>,
+    pub address: SocketAddr,
+}
+
+impl Display for ConnectionInfo {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "redis://{}:{}@{}",
+            if let Some(ref user) = self.user {
+                user.as_ref()
+            } else {
+                ""
+            },
+            if let Some(ref pass) = self.password {
+                pass.as_ref()
+            } else {
+                ""
+            },
+            self.address
+        )
+    }
 }
 
 impl RedisClusterClient {
@@ -14,8 +48,9 @@ impl RedisClusterClient {
     /// # Panics
     ///
     /// This function will panic if an invalid configuration is passed
-    pub fn new(addrs: &[SocketAddr]) -> Self {
-        let connection_strings = addrs.iter().map(|sa| format!("redis://{}", sa)).collect();
+    pub fn new(addrs: &[&ConnectionInfo]) -> Self {
+        let connection_strings = addrs.iter().map(|ci| ci.to_string()).collect();
+        log::trace!("Connection strings: {:?}", connection_strings);
         RedisClusterClient {
             client: Client::open(connection_strings).expect("Clusterclient created"),
         }
@@ -37,7 +72,15 @@ impl Storage for RedisClusterClient {
         Vec<trust_dns_server::client::rr::LowerName>,
         Box<dyn std::error::Error + Send + Sync>,
     > {
-        unimplemented!();
+        let mut con = self.client.get_connection().await?;
+        Ok(con
+            .scan_match("zone:*")
+            .await?
+            .filter_map(|key: String| async move {
+                LowerName::from_str(key.trim_start_matches("zone:")).ok()
+            })
+            .collect::<Vec<LowerName>>()
+            .await)
     }
 
     async fn lookup_records(
@@ -47,6 +90,11 @@ impl Storage for RedisClusterClient {
         rtype: trust_dns_proto::rr::RecordType,
     ) -> Result<Option<Vec<crate::storage::StoredRecord>>, Box<dyn std::error::Error + Send + Sync>>
     {
-        unimplemented!();
+        let mut con = self.client.get_connection().await?;
+        let data = con
+            .hget::<_, &str, Vec<u8>>(format!("resource:{}:{}", zone, name), rtype.into())
+            .await?;
+
+        Ok(Some(serde_json::from_slice(&data)?))
     }
 }
