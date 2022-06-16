e5cc9aa94272cf274021b22cb2bc0728811e2ea7
Lee Smet <lee.smet@hotmail.com>
2022-06-16 18:09:24 +0200

    Use fred instead of redis_cluster_async
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- src/redis.rs
+++ src/redis.rs
@@ -1,44 +1,22 @@
-use futures_util::StreamExt;
-use redis_cluster_async::{
-    redis::{AsyncCommands, Cmd},
-    Client,
+use futures_util::{join, stream, StreamExt};
+// use redis_cluster_async::{
+//     redis::{self, AsyncCommands},
+//     Client,
+// };
+use fred::{
+    pool::RedisPool,
+    prelude::*,
+    types::{BackpressureConfig, PerformanceConfig, RespVersion, ScanType},
 };
 use serde::Deserialize;
 use trust_dns_server::client::rr::LowerName;
 
-use std::{fmt::Display, net::SocketAddr, str::FromStr};
+use std::{net::SocketAddr, str::FromStr};
 
 use crate::storage::{Storage, StorageRecord};
 
 pub struct RedisClusterClient {
-    client: Client,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct ClusterConnectionInfo {
-    pub user: Option<String>,
-    pub password: Option<String>,
-    pub address: SocketAddr,
-}
-
-impl Display for ClusterConnectionInfo {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "redis://{}:{}@{}",
-            if let Some(ref user) = self.user {
-                user.as_ref()
-            } else {
-                ""
-            },
-            if let Some(ref pass) = self.password {
-                pass.as_ref()
-            } else {
-                ""
-            },
-            self.address
-        )
-    }
+    client: RedisPool,
 }
 
 impl RedisClusterClient {
@@ -48,19 +26,42 @@ impl RedisClusterClient {
     /// # Panics
     ///
     /// This function will panic if an invalid configuration is passed
-    pub fn new(addrs: &[&ClusterConnectionInfo]) -> Self {
-        let connection_strings = addrs.iter().map(|ci| ci.to_string()).collect();
-        log::trace!("Connection strings: {:?}", connection_strings);
-        RedisClusterClient {
-            client: Client::open(connection_strings).expect("Clusterclient created"),
-        }
+    pub fn new(username: Option<String>, password: Option<String>, addrs: &[SocketAddr]) -> Self {
+        let mut conf = RedisConfig::default();
+        conf.username = username;
+        conf.password = password;
+        conf.server = ServerConfig::Clustered {
+            hosts: addrs
+                .into_iter()
+                .map(|sa| (sa.ip().to_string(), sa.port()))
+                .collect(),
+        };
+        conf.version = RespVersion::RESP2;
+        let mut perf = PerformanceConfig::default();
+        perf.cluster_cache_update_delay_ms = 10;
+        perf.max_command_attempts = 20;
+        perf.backpressure = BackpressureConfig {
+            disable_auto_backpressure: false,
+            disable_backpressure_scaling: false,
+            min_sleep_duration_ms: 10,
+            max_in_flight_commands: 5000,
+        };
+        let client = RedisPool::new(conf, 10).expect("Valid pool config");
+        let reconnect = ReconnectPolicy::new_constant(1_000, 10);
+        let conn_task = client.connect(Some(reconnect));
+        //tokio::spawn(conn_task);
+        RedisClusterClient { client }
     }
 
     /// Test the client, to see if it can actually connect to the given node. If this fails, the
     /// client should be discarded as future operations will likely also fails.
     pub async fn test(&self) -> Result<(), Box<dyn std::error::Error>> {
-        let mut con = self.client.get_connection().await?;
-        Ok(Cmd::new().arg("PING").query_async(&mut con).await?)
+        log::trace!("Testing cluster connection");
+        self.client.wait_for_connect().await?;
+        log::trace!("Client connected - try to ping");
+        self.client.ping().await?;
+        log::trace!("Cluster connection OK");
+        Ok(())
     }
 }
 
@@ -72,15 +73,49 @@ impl Storage for RedisClusterClient {
         Vec<trust_dns_server::client::rr::LowerName>,
         Box<dyn std::error::Error + Send + Sync>,
     > {
-        let mut con = self.client.get_connection().await?;
-        Ok(con
-            .scan_match("zone:*")
-            .await?
-            .filter_map(|key: String| async move {
-                LowerName::from_str(key.trim_start_matches("zone:")).ok()
+        log::trace!("Getting zones from redis cluster");
+        let mut scan_stream = self
+            .client
+            .scan_cluster("zone:*", Some(10), Some(ScanType::String));
+        // TODO: simplify this
+        Ok(scan_stream
+            .filter_map(|result| async move {
+                let mut page = match result {
+                    Ok(page) => page,
+                    Err(e) => {
+                        log::error!("Could not get zone scan entry: {}", e);
+                        return None;
+                    }
+                };
+                if let Some(keys) = page.take_results() {
+                    return Some(
+                        keys.into_iter()
+                            .filter_map(|key| {
+                                let key = match key.into_string() {
+                                    Some(key) => key,
+                                    None => {
+                                        log::error!("Could not convert key to string");
+                                        return None;
+                                    }
+                                };
+                                match LowerName::from_str(key.trim_start_matches("zone:")) {
+                                    Ok(ln) => Some(ln),
+                                    Err(e) => {
+                                        log::error!("Ignoring invalid zone {:?}: {}", key, e);
+                                        None
+                                    }
+                                }
+                            })
+                            .collect(),
+                    );
+                };
+                None
             })
-            .collect::<Vec<LowerName>>()
-            .await)
+            .collect::<Vec<Vec<LowerName>>>()
+            .await
+            .into_iter()
+            .flatten()
+            .collect())
     }
 
     async fn lookup_records(
@@ -90,20 +125,26 @@ impl Storage for RedisClusterClient {
         rtype: trust_dns_proto::rr::RecordType,
     ) -> Result<Option<Vec<crate::storage::StorageRecord>>, Box<dyn std::error::Error + Send + Sync>>
     {
-        let mut con = self.client.get_connection().await?;
-        let data = con
-            .hget::<_, &str, Vec<u8>>(format!("resource:{}:{}", zone, name), rtype.into())
+        let data = self
+            .client
+            .hget::<Vec<_>, _, &str>(format!("resource:{}:{}", zone, name), rtype.into())
             .await?;
 
-        Ok(Some(serde_json::from_slice(&data)?))
+        if data.is_empty() {
+            Ok(None)
+        } else {
+            Ok(Some(serde_json::from_slice(&data)?))
+        }
     }
 
     async fn add_zone(
         &self,
         zone: &LowerName,
     ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-        let mut con = self.client.get_connection().await?;
-        Ok(con.set(format!("zone:{}", zone), "").await?)
+        Ok(self
+            .client
+            .set(format!("zone:{}", zone), "", None, None, false)
+            .await?)
     }
 
     async fn add_record(
@@ -111,9 +152,9 @@ impl Storage for RedisClusterClient {
         zone: &LowerName,
         record: StorageRecord,
     ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        log::debug!("Adding record {:?}", record);
         let record_name = LowerName::from(record.record.name());
         let record_type = record.record.record_type();
-        let mut con = self.client.get_connection().await?;
 
         let mut record_set = self
             .lookup_records(zone, &record_name, record_type)
@@ -124,11 +165,12 @@ impl Storage for RedisClusterClient {
         record_set.push(record);
         let new_record_set = serde_json::to_vec(&record_set)?;
 
-        Ok(con
-            .hset::<_, &str, _, _>(
+        log::trace!("record set: {:?}", new_record_set);
+        Ok(self
+            .client
+            .hset::<_, _, (&str, &[u8])>(
                 format!("resource:{}:{}", zone, record_name),
-                record_type.into(),
-                new_record_set,
+                (record_type.into(), &new_record_set),
             )
             .await?)
     }
